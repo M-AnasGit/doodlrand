6ca41ac004245d62b2235ac9d7825b2cd6cfd685
Anas <manasgithub1@gmail.com>
2025-03-23 14:46:10 +0000

    init

---
+++ src/electron/main.ts
@@ -0,0 +1,13 @@
+import { BrowserWindow, app } from 'electron';
+import path from 'path';
+import { isDev } from './util.js';
+
+app.on('ready', () => {
+    const mainWindow = new BrowserWindow();
+
+    if (isDev()) {
+        mainWindow.loadURL('http://localhost:5111');
+    } else {
+        mainWindow.loadFile(path.join(app.getAppPath(), '/dist-react/index.html'));
+    }
+});
---
+++ src/ui/app.tsx
@@ -0,0 +1,3 @@
+export default function App() {
+    return <span className="text-3xl text-foreground bg-muted">Hello world</span>;
+}
